ec530a08243061a281bac5fca3f10b0f6d06711d
Cybersoul2042 <amirsekiroguru@gmail.com>
2023-07-05 16:30:07 +0330

    Add New Project

--- djangotuto/settings.py
+++ djangotuto/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'hello',
     'newyear',
     'tasks',
+    'training',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- training/views.py
+++ training/views.py
@@ -6,28 +6,22 @@ from django import forms
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
-
 def index(request):
     if "tasks" not in request.session:
         request.session["tasks"] = []
-    return render(request, "tasks/index.html", {
-        "tasks": request.session["tasks"]
-        
-    })
-
-def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
-        
         if form.is_valid():
             task = form.cleaned_data["task"]
             request.session["tasks"] += [task]
-            return HttpResponseRedirect(reverse("tasks:index"))
+            return HttpResponseRedirect(reverse("training:index"))
         else:
-            return render(request, "tasks/add.html", {
+            return render(request, "training/index.html", {
                 "form": form
-            })   
-        
-    return render(request, "tasks/add.html", {
-        "form": NewTaskForm()
+            })
+    return render(request, "training/index.html", {
+        "form": NewTaskForm(),
+        "tasks": request.session["tasks"]
     })
+   
+    
